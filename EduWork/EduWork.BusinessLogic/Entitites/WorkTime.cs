340ec37df22fd84159dfda7c29d35899305bfcdf
Nino Lončar <nino.loncar16@gmail.com>
2024-05-13 17:06:23 +0200

    Add WorkTime entity class

---
+++ EduWork/EduWork.BusinessLogic/Entitites/WorkTime.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EduWork.BusinessLogic.Entitites
+{
+    [Table("WorkTimes")]
+    [Index(nameof(Date))]
+    [Index(nameof(UserId))]
+    public class WorkTime
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public DateOnly Date { get; set; }
+
+        [Required]
+        public TimeOnly StartTime { get; set; }
+
+        [Required]
+        public TimeOnly EndTime { get; set;}
+
+        public User User { get; set; }
+    }
+}
