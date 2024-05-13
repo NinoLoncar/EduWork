e6bc5b820dc402733c8ac3c271393f7fb7136bba
Nino Lončar <nino.loncar16@gmail.com>
2024-05-13 17:06:55 +0200

    Add ProjectTime entity class

---
+++ EduWork/EduWork.BusinessLogic/Entitites/ProjectTime.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EduWork.BusinessLogic.Entitites
+{
+
+    [Table("ProjectTimes")]
+    [Index(nameof(UserId))]
+    [Index(nameof(ProjectId))]
+    public class ProjectTime
+    {
+        [Key]
+        public Guid UserId { get; set; }
+
+        [Key]
+        public Guid ProjectId { get; set; }
+
+        public string Comment { get; set; }
+
+        [Required]
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        public DateOnly Date { get; set; }
+
+        public User User { get; set; }
+        
+        public Project Project { get; set; }
+    }
+}
