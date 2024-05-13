6efdef3657e36c77f5f2e2db6afb005d41dbf286
Nino Lončar <nino.loncar16@gmail.com>
2024-05-13 17:05:35 +0200

    Add Project role enitity class

---
+++ EduWork/EduWork.BusinessLogic/Entitites/ProjectRole.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EduWork.BusinessLogic.Entitites
+{
+    [Table("ProjectRoles")]
+    public class ProjectRole
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<Project> Projects { get; set; }
+    }
+}
