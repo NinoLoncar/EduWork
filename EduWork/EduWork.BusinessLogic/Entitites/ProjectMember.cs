9e8f623668990123aaacd240ebe32cc29fef06bd
Nino Lončar <nino.loncar16@gmail.com>
2024-05-13 17:06:43 +0200

    Add ProjectMember entity class

---
+++ EduWork/EduWork.BusinessLogic/Entitites/ProjectMember.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EduWork.BusinessLogic.Entitites
+{
+    [Table("ProjectMembers")]
+    public class ProjectMember
+    {
+        [Key]
+        public Guid UserId { get; set; }
+        [Key]
+        public Guid ProjectId { get; set; }
+
+        [Required]
+        public Guid ProjectRoleId { get; set; }
+
+
+        public User User { get; set; }
+
+
+        public Project Project { get; set; }
+
+        public ProjectRole ProjectRole { get; set; }
+    }
+}
