49feabc9e073067ede36102c99a5ef60e3a7d65d
Nino Lončar <nino.loncar16@gmail.com>
2024-05-13 23:52:22 +0200

    Add ApplicationDbContext

---
+++ EduWork/EduWork.Data/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using EduWork.Domain.Entitites;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduWork.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<WorkTime> WorkTimes { get; set; }
+
+        public DbSet<Role> Roles { get; set; }
+
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<ProjectCategory> ProjectCategories { get; set; }
+
+        public DbSet<ProjectTime> ProjectTimes { get; set; }
+
+        public DbSet<ProjectRole> ProjectRole { get; set; }
+
+        public DbSet<ProjectMember> ProjectMembers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProjectMember>()
+                        .HasKey(e => new { e.UserId, e.ProjectId });
+
+            modelBuilder.Entity<ProjectTime>()
+                        .HasKey(e => new { e.UserId, e.ProjectId });
+        }
+    }
+}
