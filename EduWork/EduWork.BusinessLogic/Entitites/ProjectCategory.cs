e9d4a587ff1df2191b83575aa6ce9f1e858f01b7
Nino Lončar <nino.loncar16@gmail.com>
2024-05-13 17:06:12 +0200

    Add ProjectCategory entity class

---
+++ EduWork/EduWork.BusinessLogic/Entitites/ProjectCategory.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EduWork.BusinessLogic.Entitites
+{
+    [Table("ProjectCategories")]
+    public class ProjectCategory
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<Project> Projects { get; set; }
+    }
+}
