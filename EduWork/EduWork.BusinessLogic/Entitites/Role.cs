798651f8cbaab36f8b24eed35dfac3fe91e9250f
Nino Lončar <nino.loncar16@gmail.com>
2024-05-13 17:05:20 +0200

    Add Role enitity classes

---
+++ EduWork/EduWork.BusinessLogic/Entitites/Role.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EduWork.BusinessLogic.Entitites
+{
+    [Table("Roles")]
+    public class Role
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<User> Users { get; set;}
+    }
+}
