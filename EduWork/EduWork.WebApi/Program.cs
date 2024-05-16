f550e56c21e318c6c1e0574f26801869308cf721
Nino Lončar <nino.loncar16@gmail.com>
2024-05-16 22:05:34 +0200

    Add authorization to Swagger

--- EduWork/EduWork.WebApi/Program.cs
+++ EduWork/EduWork.WebApi/Program.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,12 +15,51 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+ 
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "EduWork", Version = "v1" });
+    c.AddSecurityDefinition(
+        "OAuth2",
+        new OpenApiSecurityScheme
+        {
+
+            Type = SecuritySchemeType.OAuth2,
+            Flows = new OpenApiOAuthFlows
+            {
+                AuthorizationCode = new OpenApiOAuthFlow
+                {
+                    TokenUrl = new Uri(builder.Configuration["SwaggerAd:TokenUrl"]),
+                    AuthorizationUrl = new Uri(builder.Configuration["SwaggerAd:AuthorizationUrl"]),
+                    Scopes = new Dictionary<string, string>
+                    {
+                    { builder.Configuration["SwaggerAd:Scope"], "Access API as User" }
+                    }
+                },
+            },
+
+        }
+
+       );
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference{Type=ReferenceType.SecurityScheme,Id="OAuth2"}
+            },
+            new[]{builder.Configuration["SwaggerAd:Scope"]
+            }
+        }
+    });
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+
 
 var app = builder.Build();
 
@@ -26,12 +67,19 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.OAuthClientId(builder.Configuration["SwaggerAd:ClientId"]);
+        c.OAuthUsePkce();
+        c.OAuthScopeSeparator(" ");
+    });
 }
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthentication();
+
+//app.UseAuthorization();
 
 app.MapControllers();
 
