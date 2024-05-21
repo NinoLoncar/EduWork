f645198c23ef9edd207c1a514a4b6f993983572a
nloncar-foi <nino.loncar16@gmail.com>
2024-05-21 22:51:54 +0200

    Add SwaggerAuthorizationConfiguration class

---
+++ EduWork/EduWork.WebApi/Configuration/SwaggerAuthorizationConfiguration.cs
@@ -0,0 +1,15 @@
+﻿namespace EduWork.WebApi.Configuration
+{
+    public class SwaggerAuthorizationConfiguration
+    {
+        public const string Section = "SwaggerAuthorization";
+
+        public string ClientId { get; set; }
+
+        public string TokenUrl { get; set; }
+
+        public string AuthorizationUrl { get; set; }
+
+        public string Scope { get; set; }
+    }
+}
