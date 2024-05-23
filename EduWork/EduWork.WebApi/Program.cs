ce4d31a0ec0d9c308279d2d409629b60989fdd20
nloncar-foi <nino.loncar16@gmail.com>
2024-05-23 20:30:42 +0200

    Delete forgotten comments

--- EduWork/EduWork.WebApi/Program.cs
+++ EduWork/EduWork.WebApi/Program.cs
@@ -44,9 +44,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-//app.UseAuthentication();
+app.UseAuthentication();
 
-//app.UseAuthorization();
+app.UseAuthorization();
 
 app.MapControllers();
 
