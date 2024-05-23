d0f8ce8d9a345f5592ec499d8c46c72a48be124f
nloncar-foi <nino.loncar16@gmail.com>
2024-05-23 18:53:38 +0200

    Add ApplicationDbContextFactory

---
+++ EduWork/EduWork.Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EduWork.Data
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\Database;Initial Catalog=EduWork;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
