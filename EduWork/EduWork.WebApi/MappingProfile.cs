327387f0e69ad983a71bac3b663347701abfec4c
nloncar-foi <nino.loncar16@gmail.com>
2024-05-24 20:00:06 +0200

    Add MappingProfile

--- EduWork/EduWork.WebApi/MappingProfile.cs
+++ EduWork/EduWork.WebApi/MappingProfile.cs
@@ -1,6 +1,15 @@
-﻿namespace EduWork.WebApi
+﻿using AutoMapper;
+using EduWork.Common.DTOs;
+using EduWork.Domain.Entitites;
+
+namespace EduWork.WebApi
 {
-    public class MappingProfile
+    public class MappingProfile:Profile
     {
+        public MappingProfile() {
+
+            CreateMap<WorkTimeDTO, WorkTime>();
+            CreateMap<WorkTime, WorkTimeDTO>();
+        }
     }
 }
