a3d820550251169017ac5ec1cfb967f57448edfa
nloncar-foi <nino.loncar16@gmail.com>
2024-05-27 17:47:54 +0200

    Add WorkTime.razor

--- EduWork/EduWork.UI/Program.cs
+++ EduWork/EduWork.UI/Program.cs
@@ -6,11 +6,12 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7289") });
 
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add(builder.Configuration["AzureAd:Scope"]);
 });
 
 await builder.Build().RunAsync();
